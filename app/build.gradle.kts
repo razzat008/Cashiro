d93d4e569ccb16c721932733d59e4dd987142a5c
Ritesh kanwar <riteshkanwar0309@gmail.com>
2026-03-11 16:43:31 +0530

    chore: bump to v2.1.60

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ritesh.cashiro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 92
-        versionName = "2.1.57"
+        versionCode = 93
+        versionName = "2.1.60"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
